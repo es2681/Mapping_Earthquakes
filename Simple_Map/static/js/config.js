339b511e4520e6424e6bee8686db19942c69b0fb
es2681 <94587007+es2681@users.noreply.github.com>
2022-03-16 11:58:19 -0400

    Adding through Module 13.3.1

--- Simple_Map/static/js/config.js
+++ Simple_Map/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = pk.eyJ1IjoiZXMyNjgxIiwiYSI6ImNsMHM5bzA3dzAwamgzZG55NTc0cXMwbWYifQ.KQxSBsK2wScmVAKoRcIq1A
+const API_KEY = "pk.eyJ1IjoiZXMyNjgxIiwiYSI6ImNsMHM5bzA3dzAwamgzZG55NTc0cXMwbWYifQ.KQxSBsK2wScmVAKoRcIq1A"
