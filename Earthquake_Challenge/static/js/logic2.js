baa09b01567160d6c33d3ae51cd933d1fdd8b30b
es2681 <94587007+es2681@users.noreply.github.com>
2022-03-20 12:14:39 -0400

    Adding Module Challenge

--- Earthquake_Challenge/static/js/logic2.js
+++ Earthquake_Challenge/static/js/logic2.js
@@ -21,14 +21,6 @@ let baseMaps = {
     "Satellite": satelliteStreets
 };
 
-// Create the earthquake layer for our map
-let earthquakes = new L.LayerGroup();
-
-// We define an object that contains the overlays. This overlay will be visible all the time
-let overlays = {
-    Earthquakes: earthquakes
-};
-
 // Create the map object with center, zoom level, and default layer
 let map = L.map('mapid', {
     center: [39.5, -98.5],
@@ -37,16 +29,16 @@ let map = L.map('mapid', {
 });
 
 // Pass our map layers into our layers control and add the layers control to the map
-L.control.layers(baseMaps, overlays).addTo(map);
+L.control.layers(baseMaps).addTo(map);
 
 // This function returns the style data for each of the earthquakes we plot on the map. We pass the magnitude of the earthquake into a function to calculate the radius
 function styleInfo(feature) {
     return {
         opacity: 1,
         fillOpacity: 1,
-        fillColor: getColor(feature.properties.mag),
+        fillColor: "#ffae42",
         color: "#000000",
-        radius: getRadius(feature.properties.mag),
+        radius: getRadius(),
         stroke: true,
         weight: 0.5
     };
@@ -60,25 +52,6 @@ function getRadius(magnitude) {
     return magnitude * 4;
 };
 
-// This function determines the color of the circle based on the magnitude of the earthquake
-function getColor(magnitude) {
-    if (magnitude > 5) {
-        return "#ea2c2c";
-    }
-    if (magnitude > 4) {
-        return "#ea822c";
-    }
-    if (magnitude > 3) {
-        return "#ee9c00";
-    }
-    if (magnitude > 2) {
-        return "#eecc00";
-    }
-    if (magnitude > 1) {
-        return "#d4ee00";
-    }
-    return "#98ee00"
-}
 // Retrieve the earthquake GeoJSON data
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
     // Creating a GeoJSON layer with the retrieved data
@@ -88,45 +61,9 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
             console.log(data);
             return L.circleMarker(latlng);
         },
-       // We set the style for each circleMarker using our styleInfo function
-       style: styleInfo,
-       // We create a popup for each circleMarker to display the magnitude and location of the earthquake after the marker has been created and styled.
-       onEachFeature: function(feature, layer) {
-           layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-       }
-    }).addTo(earthquakes);
-    // Then we add the earthquake layer to our map
-    earthquakes.addTo(map);
+       // We set teh style for each circleMarker using our styleInfo function
+       style: styleInfo
+    }).addTo(map);
 });
 
 
-
-
-
-// Create a legend control object
-let legend = L.control({position: 'bottomright'});
-       
-// Then add all the details for the legend
-legend.onAdd = function () {
-    let div = L.DomUtil.create('div', 'info legend');
-    const magnitudes = [0, 1, 2, 3, 4, 5];
-    const colors = [
-        "#98ee00",
-        "#d4ee00",
-        "#eecc00",
-        "#ee9c00",
-        "#ea822c",
-        "#ea2c2c"
-    ];        
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < magnitudes.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + colors[i] + '"></i> ' +
-            magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
-    }
-
-    return div;
-};
-
-legend.addTo(map);
